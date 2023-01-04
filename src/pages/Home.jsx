e02abe89a7e852627df3e4a56ee71e2fe9d3179e
unknown <hitprofit70@gmail.com>
2023-01-04 16:16:21 +0200

    footer file added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
+    <>
     <Navbar/>
+    <Footer/>
+    </>
   )
 }
 
