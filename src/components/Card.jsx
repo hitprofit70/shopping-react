1bf1363433a56d0d1a30501b3b577002521c2302
unknown <hitprofit70@gmail.com>
2023-01-06 14:39:42 +0200

    card fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = () => {
     return (
         <>
-            <div className='slogan'>Great Smartphone
+            <div className='slogan'>Great Smartphones
             </div>
             <div className='container'>
                 <div className="card">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Smartphone</a>
+                    <a className="navbar-brand" href="#">NSP</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
