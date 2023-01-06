b8ca60b613aefb82e236ed5eec7ec4da3b2d4c11
unknown <hitprofit70@gmail.com>
2023-01-06 12:54:32 +0200

    DotLoader spinner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Home from './pages/Home';
+import { useState, useEffect } from "react";
+import DotLoader from "react-spinners/DotLoader";
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+
+  }, [])
+
   return (
+
+  
     <>
+    {
+
+    loading ?
+          <DotLoader color={'#070707'} loading={loading} size={100} className='spinner-DotLoader'/>
+          :
     <Home/>
+     }
     </>
   );
 }
