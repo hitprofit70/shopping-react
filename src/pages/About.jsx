d4145e18cd4be937b3850afdd92eaa0b6804b54a
unknown <hitprofit70@gmail.com>
2023-01-11 10:22:54 +0200

    card and image added in cart page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 
 const About = () => {
   return (
@@ -17,8 +16,6 @@ const About = () => {
       <div className='about-image'>
         <img src="https://images.unsplash.com/photo-1543069190-9d380c458bc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTYyfHxwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt=''/>
         <h5>NSP created the new develpoment of application over all the large world, also build several smartphones like a brand as Iphone, Honor, Samsung, Vivo and etc.  </h5>
-        
-
         </div>
     </Container>
     </>
--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -6,6 +6,20 @@ const MyCart = () => {
   return (
     <>
     <Navbar/>
+    <div className='card-cart'>
+      <div className='row'>
+        <img src='https://cdn.avo.africa/2022/11/14/12/16/dab25928-92f1-4038-ba5e-2789c7b98437' alt=''/>
+        <div className='title-cart'>Samsung Galaxy A73</div>
+        <div className='price-cart'>R10 490.00</div>
+        <button className='btn-cart'>Delete</button>
+      </div>
+
+      <div className='row'>
+        <div className='total-cart'>Total :<span> R40 000.00</span></div>
+        <div className='total-number-cart'></div>
+        <button className='btn-cart'>Confirm</button>
+      </div>
+    </div>
     <Footer/>
     </>
   )
