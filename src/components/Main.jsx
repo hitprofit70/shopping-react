3f964eda47490f3578fa397c725ebac048abf4ee
unknown <hitprofit70@gmail.com>
2023-01-23 16:16:00 +0200

    sidebar and main fixed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Main = () => {
     return (
-        <>
             <div className='main'>
                 <div className='container'>
-                    <h2>Happy Wednesday,Guest</h2>
+                    <h2>Happy Wednesday, Guest</h2>
                     <span>You have 0 to spend as you choose!</span>
 
                     <div className='card'>
@@ -32,77 +31,119 @@ const Main = () => {
                     </div>
 
                     <div className='card-detail'>
-                        <h4>Shopping Deals</h4>
-                        <span>View more</span>
+                        <h4>Shopping Deals  <span>View more</span></h4>
                         <div className='row'>
                             <div className='card-product'>
-                            <div className='product-image'>
-                                <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
-                            </div>
-                            <div className='sale-discount'>-29% off</div>
-                            <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
-                            <div className='product-brief-name'>Hirschs Small Applicances</div>
-                            <div className='product-price'>R2, 199.00</div>
-                            <div className='product-credit'>R214 p/m on credit</div>
-                            <div className='product-expired-price'>R3,099.00</div>
-                            <button className='btn-deals'>Add to cart</button>
-                            </div>
-
-                            <div className='card-product'>
-                            <div className='product-image'>
-                                <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
-                            </div>
-                            <div className='sale-discount'>-29% off</div>
-                            <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
-                            <div className='product-brief-name'>Hirschs Small Applicances</div>
-                            <div className='product-price'>R2, 199.00</div>
-                            <div className='product-credit'>R214 p/m on credit</div>
-                            <div className='product-expired-price'>R3,099.00</div>
-                            <button className='btn-deals'>Add to cart</button>
+                                <div className='product-image'>
+                                    <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
+                                </div>
+                                <div className='sale-discount'>-29% off</div>
+                                <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
+                                <div className='product-brief-name'>Hirschs Small Applicances</div>
+                                <div className='product-price'>R2, 199.00</div>
+                                <div className='product-credit'>R214 p/m on credit</div>
+                                <div className='product-expired-price'>R3,099.00</div>
+                                <button className='btn-deals'>Add to cart</button>
                             </div>
 
                             <div className='card-product'>
-                            <div className='product-image'>
-                                <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
-                            </div>
-                            <div className='sale-discount'>-29% off</div>
-                            <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
-                            <div className='product-brief-name'>Hirschs Small Applicances</div>
-                            <div className='product-price'>R2, 199.00</div>
-                            <div className='product-credit'>R214 p/m on credit</div>
-                            <div className='product-expired-price'>R3,099.00</div>
-                            <button className='btn-deals'>Add to cart</button>
-                            </div>
-
-                            <div className='card-product'>
-                            <div className='product-image'>
-                                <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
-                            </div>
-                            <div className='sale-discount'>-29% off</div>
-                            <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
-                            <div className='product-brief-name'>Hirschs Small Applicances</div>
-                            <div className='product-price'>R2, 199.00</div>
-                            <div className='product-credit'>R214 p/m on credit</div>
-                            <div className='product-expired-price'>R3,099.00</div>
-                            <button className='btn-deals'>Add to cart</button>
+                                <div className='product-image'>
+                                    <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/8/25/9/19/7c3a587a-3ed6-41b2-8288-d8a654918bf8.PNG" alt="" className="" />
+                                </div>
+                                <div className='sale-discount'>-29% off</div>
+                                <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
+                                <div className='product-brief-name'>Hirschs Small Applicances</div>
+                                <div className='product-price'>R2, 199.00</div>
+                                <div className='product-credit'>R214 p/m on credit</div>
+                                <div className='product-expired-price'>R3,099.00</div>
+                                <button className='btn-deals'>Add to cart</button>
                             </div>
 
                             <div className='card-product'>
-                            <div className='product-image'>
-                                <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/8/14/45/d720aa63-c121-403b-bf7f-08cbdaca05b5.jpg" alt="" className="" />
+                                <div className='product-image'>
+                                    <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/2022/4/5/14/38/75033e4e-ed60-45e6-8709-0f92852770de.jpg" alt="" className="" />
+                                </div>
+                                <div className='sale-discount'>-29% off</div>
+                                <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
+                                <div className='product-brief-name'>Hirschs Small Applicances</div>
+                                <div className='product-price'>R2, 199.00</div>
+                                <div className='product-credit'>R214 p/m on credit</div>
+                                <div className='product-expired-price'>R3,099.00</div>
+                                <button className='btn-deals'>Add to cart</button>
                             </div>
-                            <div className='sale-discount'>-29% off</div>
-                            <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
-                            <div className='product-brief-name'>Hirschs Small Applicances</div>
-                            <div className='product-price'>R2, 199.00</div>
-                            <div className='product-credit'>R214 p/m on credit</div>
-                            <div className='product-expired-price'>R3,099.00</div>
-                            <button className='btn-deals'>Add to cart</button>
+
+                            <div className='card-product'>
+                                <div className='product-image'>
+                                    <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/primers/2023/1/5/17/46/10993007-5bda-4c86-954c-3804485b1fcd.PNG" alt="" className="" />
+                                </div>
+                                <div className='sale-discount'>-29% off</div>
+                                <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
+                                <div className='product-brief-name'>Hirschs Small Applicances</div>
+                                <div className='product-price'>R2, 199.00</div>
+                                <div className='product-credit'>R214 p/m on credit</div>
+                                <div className='product-expired-price'>R3,099.00</div>
+                                <button className='btn-deals'>Add to cart</button>
+                            </div>
+
+                            <div className='card-product'>
+                                <div className='product-image'>
+                                    <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/primers/2022/10/26/11/35/dc39a6c6-3df8-4239-92ca-c5c56779d6ee.PNG" alt="" className="" />
+                                </div>
+                                <div className='sale-discount'>-29% off</div>
+                                <div className='product-name'>Nutribullet 900 Series - 690-000900-A</div>
+                                <div className='product-brief-name'>Hirschs Small Applicances</div>
+                                <div className='product-price'>R2, 199.00</div>
+                                <div className='product-credit'>R214 p/m on credit</div>
+                                <div className='product-expired-price'>R3,099.00</div>
+                                <button className='btn-deals'>Add to cart</button>
                             </div>
                         </div>
                     </div>
+                    <div className='small-card'>
+                        <h4>Explore our Shop</h4>
+                        <div className='row'>
+                            <span className='card-one'>
+                                <a>Home & Garden</a></span>
+                            <span className='card-one'>
+                                <a>Groceries & Household</a></span>
+                            <span className='card-one'>
+                                <a>Greenbacks Exclusive</a></span>
+                            <span className='card-one'>
+                                <a>Jewellery</a></span>
+                            <span className='card-one'>
+                                <a>Office & Stationery</a></span>
+                            <span className='card-one'><a>Deals</a></span>
+                            <span className='card-one'><a>Load Shedding</a></span>
+                            <span className='card-one'><a>Liqour</a></span>
+                            <span className='card-one'><a>Tech</a></span>
+                            <span className='card-one'><a>Fashion</a></span>
+                            <span className='card-one'><a>Gaming</a></span>
+                            <span className='card-one'><a>Beauty</a></span>
+                            <span className='card-one'><a>DIY</a></span>
+                            <span className='card-one'><a>Experince</a></span>
+                            <span className='card-one'><a>Baby</a></span>
+                            <span className='card-one'><a>Books</a></span>
+                            <span className='card-one'><a>Bags</a></span>
+                            <span className='card-one'><a>Travel</a></span>
+                            <span className='card-one'><a>Sports & Hobbies</a></span>
+                        </div>
+                    </div>
+                    <div className='takeaway-card'>
+                        <h4>Takeaway stores near you  <span>View more</span></h4>
+                        <div className='body'>
+                            <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/primers/2022/9/18/9/58/f0a3898c-91b5-4fc6-b4cf-5676c381f510.jpg" alt="" className="" />
+                        </div>
+                    </div>
+
+                    <div className='featured-stores'>
+                        <h4>Featured Stores</h4>
+                        <div className='body'>
+                            <img src="https://cdn.avo.africa/cdn-cgi/image/w=580/primers/2022/11/24/6/55/7cdb491b-f745-4cb4-ba9a-6169fdbd4bc4.png" alt="" className="" />
+                        </div>
+                    </div>
+
                 </div>
-            </div></>
+            </div>
     )
 }
 
